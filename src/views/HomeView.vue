098d6571ad16586c326c70259d31b535c68163f5
kristian-wittrup <kw@easv.dk>
2022-08-05 08:43:15 +0200

    onSnapshot + add and delete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,23 @@
   >
     <p>{{person.name}} </p>
     <p>{{person.age}} </p>
+    <p>{{person.id}} </p>
+
+     <button class="bg-orange-lighten-3" @click="clickToDeleteData(person.id)">Delete test data</button>
   </div> 
+
+
+    <v-row justify="center" no-gutters class="bg-green-lighten-3">
+      <v-col cols="12" sm="4">
+        <button class="bg-purple-lighten-3" @click="clickToAddData()">Add data</button>
+      </v-col>
+    </v-row>
+    
+  <!--    <v-row justify="center" no-gutters class="bg-green-lighten-3">
+      <v-col cols="12" sm="4">
+        <button class="bg-purple-lighten-3" @click="clickToDeleteData()">Delete test data</button>
+      </v-col>
+    </v-row> -->
 
 
 </template>
@@ -16,17 +32,50 @@
 <script setup>
 import { ref } from 'vue' 
 import { db } from "@/firebase.js";
-import { collection, getDocs } from "firebase/firestore"; 
+import { collection, /* getDocs, */ addDoc, doc, deleteDoc, onSnapshot } from "firebase/firestore"; 
+
 
 let testData = ref([])
+const testDataRef = collection(db, "teststuff")
 
-const querySnapshot = await getDocs(collection(db, "teststuff"));
-querySnapshot.forEach(doc => {
-  testData.value.push({ 
-    ...doc.data(), 
-    id: doc.id 
+//onsnapshot map docs and id to name and age
+ onSnapshot(testDataRef, (snapshot) => {
+  testData.value = snapshot.docs.map(doc => {
+    return {
+      id: doc.id,
+      ...doc.data(), 
+      // name: doc.data().name,
+      // age: doc.data().age
+    }
+  })
+})
+
+
+//onsnapshot foreach docs and id to name and age
+//  onSnapshot(testDataRef, (snapshot) => {
+//   testDataRef.value = snapshot.docs.forEach(doc => {
+//   testData.value.push({ 
+//     ...doc.data(), 
+//     id: doc.id 
+//     })
+//  })
+
+
+// get data once
+// const querySnapshot = await getDocs(collection(db, "teststuff"));
+// querySnapshot.forEach((doc) => {
+//   console.log(`${doc.id} => ${doc.data()}`);
+// });
+
+  function clickToAddData() {
+    addDoc(collection(db, "teststuff"), {
+      name: "test",
+      age: "test"
     })
- })
+  }
 
+  function clickToDeleteData(id) {
+     deleteDoc(doc(db, "teststuff", id))
+  }
 
 </script>
